9634bd35f16f8b7972b757fa8350b2adf5cd0ba6
jsarasti5801 <78062151+jsarasti5801@users.noreply.github.com>
2021-08-25 01:44:08 -0500

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,15 @@
+# John Sebastian Sarasti
+# 0845801
+# Sunday, August 29, 2021 at 11:59 pm
+# MSITM 6341
+# Assignment #1
+# Assignment Title Hello World
+print()
+print ("Hello World!")
+print()
+print("My name is John Sebastian Sarasti")
+print()
+print("I am a python student")
+print()
+print("Have a nice day!")
+print()
